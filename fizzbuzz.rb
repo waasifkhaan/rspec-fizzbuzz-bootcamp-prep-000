eaf710e999c54e8ecf102736cdd0cb42d73c8664
Wasif Khan <waasif.khaan@gmail.com>
2018-04-20 21:37:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    puts "Fizz" # Go fizz
+  end
+elsif int % 5 == 0 
+puts "Buzz"
+elseif int int % 5 == 0 && int % 3 == 0
+puts "FizzBuzz"
+else
+  puts "nil"
 end
